fef3e24f17ab376a8dc07c6f4f32e34966b39591
Puci123 <michaploc@wp.pl>
2021-05-15 17:07:44 +0200

    Horizontal movment & colision detecion

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -9,8 +9,9 @@
 
 using namespace sf;
 
-Vector2i boardSize(20, 20);
-Vector2f cellSize(30, 30);
+Vector2i boardSize(12, 22);
+Vector2f boardOffset(40, 40);
+Vector2f cellSize(15, 15);
 Tetramino tetraminos[7];
 
 int* board = nullptr;
@@ -93,6 +94,20 @@ void ClearTetramino(Vector2i pos, int* tetramino)
     }
 }
 
+bool ColisionDetecd(Vector2i pos, int* tetramino) 
+{
+    for (int y = 0; y < 4; y++)
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            if (tetramino[y * 4 + x] * board[(pos.y + y) * boardSize.x + x + pos.x] > 0)
+                return true;
+        }
+
+    }
+
+    return false;
+}
 
 int main()
 {
@@ -101,17 +116,32 @@ int main()
     RenderWindow window(VideoMode(600, 600), "Tetris"); //Create window
     window.setFramerateLimit(60);
     
-    board = new int[boardSize.x * boardSize.y];         //create paly board 0 empty, 1 filled
+    board = new int[boardSize.x * boardSize.y];         //create paly board 0 empty, 1 tetramino , 2 wall
 
     //fil board
-    for (int i = 0; i < boardSize.x * boardSize.y; i++) { board[i] = 0;}
+    for (int y = 0; y < boardSize.y; y++)
+    {
+        for (int x = 0; x < boardSize.x; x++)
+        {
+
+            if (y == boardSize.y - 1 || x == 0 || x == boardSize.x - 1)
+            {
+                board[y * boardSize.x + x] = 2;
+            }
+            else
+            {
+                board[y * boardSize.x + x] = 0;
+            }
+        }
+    }
 
    
     CreateTetraminos();
 
     //Game varible
     Tetramino curent = tetraminos[0];
-    
+    curent.SetPosition(Vector2i( 2, 0));
+
     //Timing
     int sleepTime = 35;
     int tetaminoStop = 20;
@@ -125,13 +155,16 @@ int main()
     //Game loop
     while (window.isOpen())
     {
-       
+     
+        Vector2i traslation(0, 0);
+        bool roatate = false;
+
         //--------Timing--------//
         std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
         counter += 1;
 
         //--------Input--------//
-
+       
         //Pool event
         Event event;
         while (window.pollEvent(event))
@@ -141,12 +174,40 @@ int main()
                 window.close();
         }
 
-        //--------Update--------//
         ClearTetramino(curent.GetPositon(), curent.GetTetramino());
+
+
+        //--------Move handle--------//
+        if (Keyboard::isKeyPressed(Keyboard::Right))
+        {
+            std::cout << "R" << std::endl;
+            if (!ColisionDetecd(curent.GetPositon() + Vector2i(1,0), curent.GetTetramino()))
+            {
+                curent.SetPosition(Vector2i(1, 0));
+            }
+            
+        }
+        else if (Keyboard::isKeyPressed(Keyboard::Left))
+        {
+            std::cout << "L" << std::endl;
+            if (!ColisionDetecd(curent.GetPositon() + Vector2i(-1, 0), curent.GetTetramino()))
+            {
+                curent.SetPosition(Vector2i(-1, 0));
+            }
+        }
+        
+        
+
+        //--------Update--------//
 
         if (counter >= tetaminoStop)
         {
-            curent.SetPosition(Vector2i(0, 1));
+            //Move down
+            if (!ColisionDetecd(curent.GetPositon() + Vector2i(0,1),curent.GetTetramino()))
+            {
+                curent.SetPosition(Vector2i(0, 1));
+            }
+            
             counter = 0;
         }
         
@@ -166,13 +227,23 @@ int main()
             {
                 if (board[y * boardSize.x + x] == 1) 
                 {
-                    RectangleShape cell(cellSize);
-                    cell.setPosition(Vector2f(x * cellSize.x, y * cellSize.y));
+                    RectangleShape cell(Vector2f(cellSize.x - 2, cellSize.y - 2));
+                    cell.setPosition(Vector2f(x * cellSize.x + boardOffset.x, y * cellSize.y + boardOffset.y));
                     cell.setFillColor(Color::Red);
                     cell.setOutlineColor(Color::Red);
 
                     window.draw(cell);
                 }
+                else if (board[y * boardSize.x + x] == 2)
+                {
+                    RectangleShape cell(Vector2f(cellSize.x, cellSize.y));
+                    cell.setPosition(Vector2f(x * cellSize.x + boardOffset.x, y * cellSize.y + boardOffset.y));
+                    cell.setFillColor(Color::Blue);
+                    cell.setOutlineColor(Color::Blue);
+
+                    window.draw(cell);
+                }
+               
             }
         }
 
